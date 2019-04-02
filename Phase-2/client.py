cf6c334f9e75ec394ace8d6c0cd5f8ca7f39b2e1
Dwarkamoye Mohanty <mdwarkamoye@gmail.com>
2019-04-02 00:21:46 -0500

    Modified

--- Phase-2/client.py
+++ Phase-2/client.py
@@ -1,44 +1,43 @@
 import sys
-import socket
 import select
+import socket
 
 hostname = '128.171.8.120'
 buffer_size = 1024
 portnumber = 3000
- 
+
+
 def client():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-     
-    try :
-        s.connect((hostname, portnumber))
-    except :
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        soc.connect((hostname, portnumber))
+    except:
         print 'Connection not available'
         sys.exit()
-     
+
     print 'Connected to remote server'
-     
-    while 1:
-        socket_list = [sys.stdin, s]
-         
-        # Get the list sockets which are readable
-        ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
-         
-        for soc in ready_to_read:
-            if soc == s:
-                # incoming message from server, s
-                data = soc.recv(buffer_size)
-                if not data :
+
+    while True:
+        socketList = [sys.stdin, soc]
+        read, write, error = select.select(socketList, [], [])
+
+        for sock in read:
+            if sock == soc:
+                dataRecv = sock.recv(buffer_size)
+                if not dataRecv:
                     print '\nDisconnected from server'
                     sys.exit()
-                elif data == "exit":
+                elif dataRecv == "exit":
                     print ("Server closed connection")
                     sys.exit()
-                else :
-                    print("Received from server:  "+data)
-            else :
+                else:
+                    print("Received from server:  " + dataRecv)
+            else:
                 # user typed a message
                 msg = raw_input()
-                s.send(msg.encode("utf8"))
+                soc.send(msg.encode("utf8"))
+
 
 if __name__ == "__main__":
     sys.exit(client())
--- Phase-2/server.js
+++ Phase-2/server.js
@@ -8,23 +8,23 @@ server.listen(3000,'128.171.8.120', function() {
   console.log('Server listening to %j', server.address());
 });
 function handleConnection(conn) {  
-  var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;
-  console.log('New client connection from %s', remoteAddress);
+  var remoteAddr = conn.remoteAddress + ':' + conn.remotePort;
+  console.log('New client connection from %s', remoteAddr);
   conn.setEncoding('utf8');
   conn.on('data', onConnData);
   conn.once('close', onConnClose);
   conn.on('error', onConnError);
   function onConnData(data) {
-    console.log('Connection data from %s: %j', remoteAddress, data);
+    console.log('Connection data from %s: %j', remoteAddr, data);
     for (var i = 0; i < sockets.length; i++){
             if (sockets[i] === conn) continue;
             sockets[i].write(data.toString());
         }
   }
   function onConnClose() {
-    console.log('Connection from %s closed', remoteAddress);
+    console.log('Connection from %s closed', remoteAddr);
   }
-  function onConnError(err) {
-    console.log('Connection %s error: %s', remoteAddress, err.message);
+  function onConnError(error) {
+  console.log('Error %s connection: %s', remoteAddr, error.message);
   }
 }
